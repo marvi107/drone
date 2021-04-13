a8a1a90d57ff759c213a09c16beaf0cb54f8ec76
Marvi Jovani <jovani.marvi@gmail.com>
2021-04-13 17:42:34 +0200

    hmm

--- drone.rb
+++ drone.rb
@@ -1,3 +1,17 @@
 puts "Hi from above"
+
 puts "Hello from master through stone"
+
 puts "hello222222"
+
+
+puts "Hello from master through stone"
+
+puts "hi"
+
+puts "Hello again"
+
+puts "Hello from downtown"
+
+
+
